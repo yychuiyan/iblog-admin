50f349224e3eb2c64d09abbe0ae408601c7b6c6d
yychuiyan <yychuiyan@outlook.com>
2023-05-11 15:34:55 +0800

    feat:服务部署

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import "@arco-design/web-react/dist/css/arco.css";
 import App from './App';
-// 主入口
+// 主入口文件
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
